3a4fcdecab66fd8cd55ad37ba6842172be41c9c9
daniel valle <danielvallec98@gmail.com>
2025-06-27 15:22:09 -0600

    actualizar

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -36,7 +36,7 @@ const FeedbackForm = () => {
 
   return (
     <section className="max-w-md mx-auto mt-12 p-6 bg-white rounded shadow-md">
-      <h3 className="text-2xl font-semibold mb-4 text-gray-900">Envíanos tu feedback</h3>
+      <h3 className="text-2xl font-semibold mb-4 text-gray-900">Envíanos Tu opinion</h3>
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label htmlFor="email" className="block mb-1 text-gray-700">Correo electrónico:</label>
